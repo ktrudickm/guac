30d2f076b9234e3ad40275e1309898023908b535
Kendall <ktrudickm@gmail.com>
2021-09-13 15:12:33 -0400

    external link adjustment

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -38,7 +38,7 @@ const Navbar = ({ toggle }) => {
                             <NavLinks to='team'>Team</NavLinks>
                         </NavItem> */}
                         <NavBtn>
-                            <NavBtnLink href='https://eepurl.com/hHIAf9' target="_blank">Stay Informed</NavBtnLink>
+                            <NavBtnLink href='http://eepurl.com/hHIAf9' target="_blank">Stay Informed</NavBtnLink>
                         </NavBtn>
                     </NavMenu>
                 </NavbarContainer>
